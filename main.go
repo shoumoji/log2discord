f1f1a7e20aaadf2e5b46aeae53c7b0734ff2c5fe
ayumin <kosyu79@gmail.com>
2021-02-05 16:49:06 +0900

    トークンとチャンネルIDを埋め込み

--- main.go
+++ main.go
@@ -5,17 +5,12 @@ import (
 	"os"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	// DiscordのBotトークンとログチャンネルを.envから取得
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file", err)
-	}
-	Token := os.Getenv("TOKEN")
-	logChannelID := os.Getenv("logChannelID")
+	Token := ""
+	logChannelID := ""
 	// どのディレクトリをDiscordに出力するか引数で選択
 	sendFile := os.Args[1]
 
